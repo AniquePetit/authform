32109d50a3214fed63a5cbfae17e5df27575b539
AniquePetit <aniquepetit@hotmail.com>
2025-11-11 11:43:17 +0100

    Prepare for GitHub Pages deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,22 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {
+      colors: {
+        'blue-950': '#000814',
+        'blue-900': '#001d3d',
+        'blue-800': '#003566',
+        'yellow-500': '#ffc300',
+        'yellow-400': '#ffb608',
+        'yellow-300': '#ffe345',
+      },
+      fontFamily: {
+        prompt: ['Prompt', 'sans-serif'],
+      },
       keyframes: {
         blob: {
           '0%': { transform: 'translate(0px,0px) scale(1)' },
@@ -15,4 +31,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
